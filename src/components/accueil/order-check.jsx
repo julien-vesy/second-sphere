e7a950edf913126c3edaa5f1691019527eb16fbf
Julien Vesy <julien.vesy@smile.fr>
2025-11-21 17:40:48 +0100

    accueil

--- src/components/accueil/order-check.jsx
+++ src/components/accueil/order-check.jsx
@@ -37,9 +37,9 @@ export default function OrderCheck() {
           onClick={() => (window.location.href = '/devis')}
           class="text-white inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all outline-none focus-visible:ring-2 h-10 rounded-md px-6 bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 shadow-lg cursor-pointer active:scale-95"
         >
-          <span className="relative flex size-3">
-            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
-            <span className="relative inline-flex size-3 rounded-full bg-green-500"></span>
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-green-500"></span>
           </span>
           &nbsp;Commandes ouvertes
         </button>
@@ -50,9 +50,9 @@ export default function OrderCheck() {
           disabled
           class="text-white inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 outline-none h-10 rounded-md px-6 bg-gradient-to-r from-gray-400 to-gray-500 shadow-lg cursor-not-allowed"
         >
-          <span className="relative flex size-3">
-            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
-            <span className="relative inline-flex size-3 rounded-full bg-red-500"></span>
+          <span class="relative flex size-3">
+            <span class="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
+            <span class="relative inline-flex size-3 rounded-full bg-red-500"></span>
           </span>
           &nbsp;Commandes fermées
         </button>
