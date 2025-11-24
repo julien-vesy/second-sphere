49e50c9693d38f21c7dd81cce93394af67f7a6d0
Julien Vesy <julien.vesy@smile.fr>
2025-11-24 11:42:13 +0100

    move assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,10 @@ import tailwind from '@astrojs/tailwind'
 export default defineConfig({
   output: 'static',
   integrations: [solid(), tailwind()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+    formats: ['webp', 'avif'],
+  },
 })
